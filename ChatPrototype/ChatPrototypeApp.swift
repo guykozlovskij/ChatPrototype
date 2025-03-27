89c775cfe682de8dc1944a1aa55ad72018a0aba3
Guy Kozlovskij <80322586+guykozlovskij@users.noreply.github.com>
2025-03-27 07:51:01 +0000

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Guy Kozlovskij on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
